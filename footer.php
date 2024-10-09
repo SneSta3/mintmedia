0fdd15f30068fcb9c09f0c45d42630e67c41472b
SneSta3 <snjezastakic@gmail.com>
2024-10-09 13:04:26 +0200

    header i footer-include na sve stranice

---
+++ footer.php
@@ -0,0 +1,33 @@
+  <!-- footer --> 
+  <footer class="py-5 text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3 class="d-inline-block">O nama</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident rerum, ut dolorum id numquam quaerat qui eos non eum dicta.</p>
+        </div>
+        <div class="col-md-4">
+          <h3 class="d-inline-block">Kontakt</h3>
+          <ul class="list-unstyled contact">
+            <li class="mb-2"><i class="bi bi-pin-map-fill"></i>Glavna Ulica 24</li>
+            <li class="mb-2"><i class="bi bi-telephone-fill"></i>024 123 456</li>
+            <li class="mb-2"><i class="bi bi-envelope-fill"></i>office@media.rs</li>
+          </ul>
+      </div>
+      <div class="col-md-4">
+          <h3 class="d-inline-block">Povezimo se</h3>
+          <ul class="list-unstyled socials">
+            <li class="d-inline-block"><a href=""><i class="bi bi-facebook"></i></a></li>
+            <li class="d-inline-block"><a href=""><i class="bi bi-instagram"></i></a></li>
+            <li class="d-inline-block"><a href=""><i class="bi bi-linkedin"></i></a></li>
+          </ul>
+      </div>
+    </div>
+   </footer>
+
+ 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
