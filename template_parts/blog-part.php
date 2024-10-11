b48d2932a346ec495ebee576ef8eb376cae94b8a
SneSta3 <snjezastakic@gmail.com>
2024-10-11 19:10:34 +0200

    gotov dinamicki sajt

--- template_parts/blog-part.php
+++ template_parts/blog-part.php
@@ -1,40 +1,30 @@
+<?php 
+//debagovanje (ocitaj_blog_text_file());
+//normalizacija_blog_podataka(ocitaj_blog_text_file());
+$blog_postovi = izlistaj_sve_blogove();
+//debagovanje($blog_postovi);
+?>   
+   
    <!-- blog -->
    <main class="container">
         <div class="row">
             <section class="col-md-8">
+
+            <?php foreach($blog_postovi as $post) : ?>
                 <article>
                     <div class="featured-image">
-                        <img class="w-100" src="img/mint drustvene mreze.jpg" alt="">
+                        <img class="w-100" src="<?php echo $post['slika']; ?>" alt="<?php echo $post['naslov']; ?>">
                     </div>
                     <div class="text position-relative p-5 text-white">
-                        <h3>Drustvene mreze</h3>
-                        <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.9.2024. | Admin</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis, aperiam porro mollitia ipsum minima sequi, aspernatur earum ad officiis facere illum dolorum maxime error laboriosam, optio pariatur quod dignissimos beatae. Unde quisquam rerum aperiam maiores vitae eveniet! Vero officiis impedit consequatur facilis corporis aliquam! Voluptate ab beatae ea minus?</p>
+                        <h3><?php echo $post['naslov']; ?></h3>
+                        <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3"><?php echo $post['datum']; ?> | <?php echo $post['admin']; ?></span>
+                        <p><?php echo $post['tekst']; ?></p>
                         <a href="" class="btn btn-light">Ceo tekst</a>
                     </div>
                 </article>
-                <article>
-                    <div class="featured-image">
-                        <img class="w-100" src="img/mint_seo.jpg" alt="">
-                    </div>
-                    <div class="text position-relative p-5 text-white">
-                        <h3>Search engine optimizacija</h3>
-                        <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.9.2024. | Admin</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis, aperiam porro mollitia ipsum minima sequi, aspernatur earum ad officiis facere illum dolorum maxime error laboriosam, optio pariatur quod dignissimos beatae. Unde quisquam rerum aperiam maiores vitae eveniet! Vero officiis impedit consequatur facilis corporis aliquam! Voluptate ab beatae ea minus?</p>
-                        <a href="" class="btn btn-light">Ceo tekst</a>
-                    </div>
-                </article>
-                <article>
-                    <div class="featured-image">
-                        <img class="w-100" src="img/mint-google-oglasi.jpg" alt="">
-                    </div>
-                    <div class="text position-relative p-5 text-white">
-                        <h3>Google oglasi</h3>
-                        <span class="bg-dark text-white d-inline-block py-1 px-3 mb-3">18.9.2024. | Admin</span>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil blanditiis, aperiam porro mollitia ipsum minima sequi, aspernatur earum ad officiis facere illum dolorum maxime error laboriosam, optio pariatur quod dignissimos beatae. Unde quisquam rerum aperiam maiores vitae eveniet! Vero officiis impedit consequatur facilis corporis aliquam! Voluptate ab beatae ea minus?</p>
-                        <a href="" class="btn btn-light">Ceo tekst</a>
-                    </div>
-                </article>
+            <?php endforeach; ?>    
+                
+
             </section>
             <aside class="col-md-4">
             <?php include 'template_parts/sidebar.php';?>
